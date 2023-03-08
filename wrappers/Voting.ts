22bc519699cde3bdb99f5e176aee044cf57f6bd1
EmelyanenkoK <you@example.com>
2023-03-08 16:13:33 +0300

    Fix voting wrapper

--- wrappers/Voting.ts
+++ wrappers/Voting.ts
@@ -6,15 +6,45 @@ export class Voting implements Contract {
     static createFromAddress(address: Address) {
         return new Voting(address);
     }
-
+/*
+    return (init, executed,
+            dao_address, initiator,
+            voting_id, expiration_date, voting_type,
+            ;; proposal
+            minimal_execution_amount, message, description,
+            voted_for, voted_against);
+*/
+    async getData(provider: ContractProvider) {
+        let res = await provider.get('get_voting_data', []);
+        console.log(res.stack);
+        let init = res.stack.readBoolean();
+        let executed = res.stack.readBoolean();
+        let daoAddress = res.stack.readAddress();
+        let initiator = res.stack.readAddress();
+        let votingId = res.stack.readBigNumber();
+        let expirationDate = res.stack.readBigNumber();
+        let votingType = res.stack.readBigNumber();
+        let minAmount = res.stack.readBigNumber();
+        let message = res.stack.readCellOpt();
+        let description = res.stack.readString();
+        let votedFor = res.stack.readBigNumber();
+        let votedAgainst = res.stack.readBigNumber();
+        return {
+            init, executed,
+            daoAddress, initiator,
+            votingId, expirationDate, votingType,
+            minAmount, message, description,
+            votedFor, votedAgainst,
+        };
+    }
 /*
 (init, dao_address, voting_id, expiration_date, voting_type,
             proposal, wallet_code,
             voted_for, voted_against,
             executed, initiator);
 */
-    async getData(provider: ContractProvider) {
-        let res = await provider.get('get_voting_data', []);
+    async getFullData(provider: ContractProvider) {
+        let res = await provider.get('get_full_voting_data', []);
         let init = res.stack.readBoolean();
         let daoAddress = res.stack.readAddress();
         let votingId = res.stack.readBigNumber();
