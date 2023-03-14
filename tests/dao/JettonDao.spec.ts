751c62db2bd461a53b97ad81820c7f398cf27dab
Trinketer22 <trinketer22@localhost>
2023-03-14 12:55:55 +0300

    voteCtx added

--- tests/dao/JettonDao.spec.ts
+++ tests/dao/JettonDao.spec.ts
@@ -1,5 +1,5 @@
 import { Blockchain, SandboxContract, TreasuryContract, Verbosity, internal } from '@ton-community/sandbox';
-import { Cell, toNano, beginCell, Address, SendMode } from 'ton-core';
+import { Cell, toNano, beginCell, Address, SendMode, OpenedContract } from 'ton-core';
 import { JettonWallet } from '../../wrappers/JettonWallet';
 import { JettonMinter } from '../../wrappers/JettonMinter';
 import { Voting } from '../../wrappers/Voting';
@@ -7,7 +7,12 @@ import { VoteKeeper } from '../../wrappers/VoteKeeper';
 import '@ton-community/test-utils';
 import { compile } from '@ton-community/blueprint';
 import { getRandom, getRandomExp, getRandomInt, getRandomTon, randomAddress, renewExp } from "../utils";
-import { exists } from 'fs';
+
+type voteCtx = {
+    init: boolean,
+    votedFor: bigint,
+    votedAgainst: bigint
+};
 
 
 describe('Votings', () => {
@@ -22,10 +27,11 @@ describe('Votings', () => {
     let initialUser1Balance:bigint;
     let initialUser2Balance:bigint;
     let initialUser3Balance:bigint;
+    let votes:voteCtx[] = []; // Array index is voting index
     let DAO:SandboxContract<JettonMinter>;
-    let userWallet:any;
-    let votingContract:any;
-    let voteKeeperContract:any;
+    let userWallet:(address:Address) => Promise<SandboxContract<JettonWallet>>;
+    let votingContract:(voting_id:bigint) => Promise<SandboxContract<Voting>>;
+    let voteKeeperContract:(wallet:SandboxContract<JettonWallet>, keeper_addr:Address) => Promise<SandboxContract<VoteKeeper>>;
     let defaultContent:Cell;
     let expirationDate:bigint;
 
@@ -38,9 +44,9 @@ describe('Votings', () => {
         user1 = await blockchain.treasury('user1');
         user2 = await blockchain.treasury('user2');
         user3 = await blockchain.treasury('user3');
-        initialUser1Balance = toNano('777');
-        initialUser2Balance = toNano('333');
-        initialUser3Balance = toNano('105');
+        initialUser1Balance = getRandomTon(100, 1000);
+        initialUser2Balance = getRandomTon(100, 1000);
+        initialUser3Balance = getRandomTon(100, 1000);
         defaultContent = beginCell().endCell();
         DAO = blockchain.openContract(
                    await JettonMinter.createFromConfig(
@@ -62,7 +68,12 @@ describe('Votings', () => {
                             await DAO.getVotingAddress(voting_id)
                           )
                      );
-        //voteKeeperContract = TODO
+        voteKeeperContract = async (jw:SandboxContract<JettonWallet>, voting_addr:Address) => blockchain.openContract(
+            VoteKeeper.createFromAddress(
+                await jw.getVoteKeeperAddress(voting_addr)
+            )
+        );
+
         await DAO.sendDeploy(user1.getSender(), toNano('1'));
         await DAO.sendMint(user1.getSender(), user1.address, initialUser1Balance, toNano('0.05'), toNano('1'));
         await DAO.sendMint(user1.getSender(), user2.address, initialUser2Balance, toNano('0.05'), toNano('1'));
@@ -70,7 +81,8 @@ describe('Votings', () => {
     });
     it('should create new voting', async () => {
             expirationDate = getRandomExp();
-            let voting = await votingContract(0n);
+            const votingId = 0n;
+            let voting = await votingContract(votingId);
 
             let createVoting = await DAO.sendCreateVoting(user1.getSender(),
                 expirationDate,
@@ -87,15 +99,28 @@ describe('Votings', () => {
                                          .storeAddress(voting.address) //voting_code
                                          .endCell()
                     });
-        });
-    it('jetton owner can vote', async () => {
-            let voting = await votingContract(0n);
-            let votingData = await voting.getData();
+
+            const votingData = await voting.getData();
+
+            votes[0] = votingData;
+
+            expect(votingData.votingId).toEqual(votingId);
+            expect(votingData.daoAddress.equals(DAO.address)).toBeTruthy();
+            expect(votingData.executed).toBe(false);
+            expect(votingData.expirationDate).toEqual(expirationDate);
+            expect(votingData.initiator.equals(user1.address)).toBeTruthy();
             expect(votingData.init).toEqual(true);
             expect(votingData.votedFor).toEqual(0n);
             expect(votingData.votedAgainst).toEqual(0n);
+    });
+
+    it('jetton owner can vote for', async () => {
+            let voting = await votingContract(0n);
+
             let votingCode = await DAO.getVotingCode();
             const user1JettonWallet = await userWallet(user1.address);
+            const keepR    = await voteKeeperContract(user1JettonWallet, voting.address)
+            const voteCtx  = votes[0];
             let voteResult = await user1JettonWallet.sendVote(user1.getSender(), voting.address, expirationDate, true, false);
             expect(voteResult.transactions).toHaveTransaction({ //notification
                         from: voting.address,
@@ -103,12 +128,56 @@ describe('Votings', () => {
                         // excesses 0xd53276db, query_id
                         body: beginCell().storeUint(0xd53276db, 32).storeUint(0, 64).endCell()
                     });
-            votingData = await voting.getData();
-            expect(votingData.init).toEqual(true);
+
+
+            voteCtx.votedFor += initialUser1Balance;
+
+            const keeperData = await keepR.getData();
+
+            expect(keeperData.voter_wallet.equals(user1JettonWallet.address)).toBeTruthy();
+            expect(keeperData.voting.equals(voting.address)).toBeTruthy();
+            expect(keeperData.votes).toEqual(voteCtx.votedFor);
+
+            const votingData = await voting.getData();
+
+            expect(votingData.init).toEqual(voteCtx.init);
             expect(votingData.votedFor).toEqual(initialUser1Balance);
-            expect(votingData.votedAgainst).toEqual(0n);
-            expect(await user1JettonWallet.getJettonBalance()).toEqual(0n);
+            expect(votingData.votedAgainst).toEqual(voteCtx.votedAgainst);
+            expect(await user1JettonWallet.getJettonBalance()).toEqual(voteCtx.votedAgainst);
+
+
         });
+
+        it('jetton owner can vote against', async () => {
+
+            let voting     = await votingContract(0n);
+            let votingData = await voting.getData();
+            let voteCtx    = votes[0];
+
+            expect(votingData.init).toEqual(voteCtx.init);
+            expect(votingData.votedFor).toEqual(voteCtx.votedFor);
+            expect(votingData.votedAgainst).toEqual(voteCtx.votedAgainst);
+
+            const user3JettonWallet = await userWallet(user3.address);
+            const voteRes           = await user3JettonWallet.sendVote(user3.getSender(), voting.address, expirationDate, false, false); 
+
+            expect(voteRes.transactions).toHaveTransaction({ //notification
+                        from: voting.address,
+                        to: user3.address,
+                        // excesses 0xd53276db, query_id
+                        body: beginCell().storeUint(0xd53276db, 32).storeUint(0, 64).endCell()
+                    });
+
+
+            voteCtx.votedAgainst += initialUser3Balance;
+            
+            votingData     = await voting.getData();
+            expect(votingData.init).toEqual(voteCtx.init);
+            expect(votingData.votedFor).toEqual(voteCtx.votedFor);
+            expect(votingData.votedAgainst).toEqual(initialUser3Balance);
+ 
+        });
+
         it('jetton owner can not transfer just after voting', async () => {
             const user1JettonWallet = await userWallet(user1.address);
             let transferResult = await user1JettonWallet.sendTransfer(user1.getSender(), toNano('0.1'), //tons
@@ -120,6 +189,7 @@ describe('Votings', () => {
                         exitCode: 706 //error::not_enough_jettons = 706;
                     });
         });
+
         it('jetton owner can transfer tokens which did not vote', async () => {
             const user2JettonWallet = await userWallet(user2.address);
             await user2JettonWallet.sendTransfer(user2.getSender(), toNano('0.15'), //tons
@@ -143,8 +213,10 @@ describe('Votings', () => {
                     });
             expect(await user1JettonWallet.getJettonBalance()).toEqual(1n);
         });
+
         it('jetton owner can vote second time but only with new jettons', async () => {
             let voting     = await votingContract(0n);
+            const voteCtx  = votes[0];
             let votingCode = await DAO.getVotingCode();
             const user1JettonWallet = await userWallet(user1.address);
             let voteResult = await user1JettonWallet.sendVote(user1.getSender(), voting.address, expirationDate, false, false);
@@ -154,10 +226,14 @@ describe('Votings', () => {
                         // excesses 0xd53276db, query_id
                         body: beginCell().storeUint(0xd53276db, 32).storeUint(0, 64).endCell()
                     });
-            let votingData = await voting.getData();
-            expect(votingData.init).toEqual(true);
-            expect(votingData.votedFor).toEqual(initialUser1Balance);
-            expect(votingData.votedAgainst).toEqual(1n);
+
+            const votingData = await voting.getData();
+            
+            voteCtx.votedAgainst += 1n;
+
+            expect(votingData.init).toEqual(voteCtx.init);
+            expect(votingData.votedFor).toEqual(voteCtx.votedFor);
+            expect(votingData.votedAgainst).toEqual(voteCtx.votedAgainst);
             expect(await user1JettonWallet.getJettonBalance()).toEqual(0n);
         });
 
@@ -183,12 +259,18 @@ describe('Votings', () => {
 
             });
             let votingData = await voting.getData();
+
             expect(votingData.init).toEqual(true);
             expect(votingData.votedFor).toEqual(0n);
             expect(votingData.votedAgainst).toEqual(0n);
-            let votingCode = await DAO.getVotingCode();
+
+            const voteCtx  = votingData as voteCtx;
+            votes[1]       = voteCtx;
+
+
             const user1JettonWallet = await userWallet(user1.address);
             let voteResult = await user1JettonWallet.sendVote(user1.getSender(), voting.address, expirationDate, true, false);
+
             expect(voteResult.transactions).toHaveTransaction({ //notification
                         from: voting.address,
                         to: user1.address,
@@ -196,12 +278,16 @@ describe('Votings', () => {
                         body: beginCell().storeUint(0xd53276db, 32).storeUint(0, 64).endCell()
                     });
 
+            voteCtx.votedFor += initialUser1Balance + 1n;
+
             votingData = await voting.getData();
-            expect(votingData.init).toEqual(true);
-            expect(votingData.votedFor).toEqual(initialUser1Balance + 1n);
-            expect(votingData.votedAgainst).toEqual(0n);
+
+            expect(votingData.init).toEqual(voteCtx.init);
+            expect(votingData.votedFor).toEqual(voteCtx.votedFor);
+            expect(votingData.votedAgainst).toEqual(voteCtx.votedAgainst);
             expect(await user1JettonWallet.getJettonBalance()).toEqual(0n);
         });
+
     it('jetton owner can vote with confirmation', async () => {
             expirationDate = renewExp(expirationDate);
             await DAO.sendCreateVoting(user1.getSender(),
@@ -212,6 +298,9 @@ describe('Votings', () => {
                 beginCell().endCell() // payload
             );
             let voting = await votingContract(2n);
+            const voteCtx  = (await voting.getData()) as voteCtx;
+            votes[2]       = voteCtx;
+
             let votingCode = await DAO.getVotingCode();
             const user1JettonWallet = await userWallet(user1.address);
             let voteResult = await user1JettonWallet.sendVote(user1.getSender(), voting.address, expirationDate, false, true);
@@ -220,25 +309,35 @@ describe('Votings', () => {
                         to: user1.address,
                         body: beginCell().storeUint(0x5fe9b8ca, 32).storeUint(0, 64).endCell()
                     });
+
+            voteCtx.votedAgainst += initialUser1Balance + 1n;
+
             let votingData = await voting.getData();
-            expect(votingData.init).toEqual(true);
-            expect(votingData.votedFor).toEqual(0n);
-            expect(votingData.votedAgainst).toEqual(initialUser1Balance + 1n);
+
+            expect(votingData.init).toEqual(voteCtx.init);
+            expect(votingData.votedFor).toEqual(voteCtx.votedFor);
+            expect(votingData.votedAgainst).toEqual(voteCtx.votedAgainst);
+
             expect(await user1JettonWallet.getJettonBalance()).toEqual(0n);
         });
+
         it('jetton balance unblocked after expiration date', async () => {
             const user1JettonWallet = await userWallet(user1.address);
+
             // await new Promise(res => setTimeout(res, Number((expirationDate + 1n) * 1000n) - Date.now()));
             // expect(await user1JettonWallet.getJettonBalance({now: expirationDate + 1n})).toEqual(initialUser1Balance + 1n);
+
             const wdata = await blockchain.runGetMethod(user1JettonWallet.address, 'get_wallet_data', [], {now: Number(expirationDate) + 1 });
             expect(wdata.stackReader.readBigNumber()).toBe(initialUser1Balance + 1n);
             // check that voting data didn't changed
-            let voting = await votingContract(0n);
+            let voting     = await votingContract(0n);
             let votingData = await voting.getData();
-            expect(votingData.init).toEqual(true);
-            expect(votingData.votedFor).toEqual(initialUser1Balance);
-            expect(votingData.votedAgainst).toEqual(1n);
+            const voteCtx  = votes[0];
+            expect(votingData.init).toEqual(voteCtx.init);
+            expect(votingData.votedFor).toEqual(voteCtx.votedFor);
+            expect(votingData.votedAgainst).toEqual(voteCtx.votedAgainst);
         });
+
         // TODO
         // check voteKeeper data in tests
 
